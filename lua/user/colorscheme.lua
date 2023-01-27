178407fc0008305a0c2efa7da213dd35e8d963bb
German Meza <germanamz@Germans-Laptop.local>
2023-01-27 13:02:01 -0600

    some customization for colorschema indentline and treesitter js go rust ts

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,4 +1,6 @@
-local colorscheme = "tokyonight-night"
+-- local colorscheme = "tokyonight-night"
+-- local colorscheme = "darkplus"
+local colorscheme = "darcula"
 
 local status_ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
 if not status_ok then
--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -1,10 +1,15 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "eol:↴"local status_ok, indent_blankline = pcall(require, "indent_blankline")
+
 if not status_ok then
   return
 end
 
 indent_blankline.setup {
   char = "▏",
+  show_end_of_line = true,
+  space_char_blankline = " ",
   show_trailing_blankline_indent = false,
   show_first_indent_level = true,
   use_treesitter = true,
